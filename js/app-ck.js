c1a17d27d30a2fba1c21bfdfec82799d91641ea7
John Wilson <john@j-wilson.com>
2013-07-24 16:49:42 -0500

    Initial commit.

---
+++ js/app-ck.js
@@ -0,0 +1,1 @@
+$(window).ready(function(){$width=$(window).width();$fontSize=$width/35;$lineHeight=$fontSize*1.45;$("body").css({"font-size":$fontSize+"px","line-height":$lineHeight+"px"})});$(window).resize(function(){$width=$(window).width();$fontSize=$width/35;$lineHeight=$fontSize*1.45;$("body").css({"font-size":$fontSize+"px","line-height":$lineHeight+"px"})});
